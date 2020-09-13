ebdc50eb3e446371b6eda11a057dfb97da43162c
zbadiru <zbadiru@live.co.uk>
2020-09-13 20:02:57 +0100

    user controller updated

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,27 @@
 class UserController < ApplicationController
-  def show
+  def sign_in
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      render json: { username: user.username, token: generate_token({ id: user.id }) }
+    else
+      render json: { message: "Failed to Sign In!"}
+    end
+  end
+
+  def validate
+    id = decode_token
+    user = User.find_by(id: id)
+
+    if user  
+      render json: { username: user.username, token: generate_token({ id: user.id }) }
+    else
+      render json: { message: "Failed to Sign In!"}
+    end
   end
 
   def new
   end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
   end
 end
