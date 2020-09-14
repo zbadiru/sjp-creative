3058a24aa6d35d1b9b61281c4183b67880103355
zbadiru <zbadiru@live.co.uk>
2020-09-14 18:59:29 +0100

    portfolio controller updated

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,22 +1,48 @@
 class PortfolioController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+
   def index
+      @portfolios = Portfolio.all
+      render json: @portfolios
   end
 
   def show
+      render json: @portfolio
   end
 
   def new
   end
 
   def create
+      @portfolio = Portfolio.new(portfolio_params)
+
+      if @portfolio.save
+          render json: @portfolio, status: :created, location: @portfolio
+      else
+          render json: @portfolio.errors, status: :unprocessable_entity
+      end
   end
 
   def edit
   end
 
   def update
+      @portfolio.update(portfolio_params)
+      render json: @portfolio
   end
 
-  def delete
+  def destroy
+      @portfolio = portfolio.destroy
+      render json: @portfolio
+  end
+
+  private 
+
+  def set_portfolio
+      @portfolio = Portfolio.find(params[:id])
+  end
+
+  def portfolio_params
+      params.require(:portfolio).permit(:photoshoot_id)
   end
 end
