d71aa56d055cb8a630270d9e300423549b42f2ac
zbadiru <zbadiru@live.co.uk>
2020-09-15 18:37:27 +0100

    photoshoot controller updated

--- app/controllers/photoshoot_controller.rb
+++ app/controllers/photoshoot_controller.rb
@@ -1,22 +1,48 @@
 class PhotoshootController < ApplicationController
+  before_action :set_photoshoot, only: [:show, :edit, :update, :destroy]
+
   def index
+      @photoshoots = Photoshoot.all
+      render json: @photoshoots
   end
 
   def show
+      render json: @photoshoot
   end
 
   def new
   end
 
   def create
+      @photoshoot = Photoshoot.new(photoshoot_params)
+
+      if @photoshoot.save
+      render json: @photoshoot, status: :created, location: @photoshoot
+      else
+      render json: @photoshoot.errors, status: :unprocessable_entity
+      end
   end
 
   def edit
   end
 
   def update
+      @photoshoot.update(photoshoot_params)
+      render json: @photoshoot
   end
 
-  def delete
+  def destroy
+      @photoshoot = photoshoot.destroy
+      render json: @photoshoot
+  end
+
+  private 
+
+  def set_photoshoot
+      @photoshoot = Photoshoot.find(params[:id])
+  end
+
+  def photoshoot_params
+      params.require(:photoshoot).permit(:name, :portfolio_id)
   end
 end
--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,48 +1,48 @@
 class PortfolioController < ApplicationController
-  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+    before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
 
-  def index
-      @portfolios = Portfolio.all
-      render json: @portfolios
-  end
+    def index
+        @portfolios = Portfolio.all
+        render json: @portfolios
+    end
 
-  def show
-      render json: @portfolio
-  end
+    def show
+        render json: @portfolio
+    end
 
-  def new
-  end
+    def new
+    end
 
-  def create
-      @portfolio = Portfolio.new(portfolio_params)
+    def create
+        @portfolio = Portfolio.new(portfolio_params)
 
-      if @portfolio.save
-          render json: @portfolio, status: :created, location: @portfolio
-      else
-          render json: @portfolio.errors, status: :unprocessable_entity
-      end
-  end
+        if @portfolio.save
+            render json: @portfolio, status: :created, location: @portfolio
+        else
+            render json: @portfolio.errors, status: :unprocessable_entity
+        end
+    end
 
-  def edit
-  end
+    def edit
+    end
 
-  def update
-      @portfolio.update(portfolio_params)
-      render json: @portfolio
-  end
+    def update
+        @portfolio.update(portfolio_params)
+        render json: @portfolio
+    end
 
-  def destroy
-      @portfolio = portfolio.destroy
-      render json: @portfolio
-  end
+    def destroy
+        @portfolio = portfolio.destroy
+        render json: @portfolio
+    end
 
-  private 
+    private 
 
-  def set_portfolio
-      @portfolio = Portfolio.find(params[:id])
-  end
+    def set_portfolio
+        @portfolio = Portfolio.find(params[:id])
+    end
 
-  def portfolio_params
-      params.require(:portfolio).permit(:photoshoot_id)
-  end
+    def portfolio_params
+        params.require(:portfolio).permit(:photoshoot_id)
+    end
 end
