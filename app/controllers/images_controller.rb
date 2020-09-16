846b9ded0fd7373b6683d1090170282b6e46ced5
zbadiru <zbadiru@live.co.uk>
2020-09-16 12:04:57 +0100

    image controller updated

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,47 @@
 class ImagesController < ApplicationController
+    before_action :set_image, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @images = Image.all
+        render json: @images
+    end
+
+    def show
+        render json: @image
+    end
+
+    def new
+    end
+
+    def create
+        @image = Image.create(image_params)
+        if @image.valid?
+            render json: @image, status: :created, location: @image
+        else
+            render json: @image.errors, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        @image.update(image_params)
+        render json: @image
+    end
+
+    def destroy
+        @image = image.destroy
+        render json: @image
+    end
+
+    private 
+
+    def set_image
+        @image = Image.find(params[:id])
+    end
+
+    def image_params
+        params.require(:image).permit(:image_url, :photoshoot_id)
+    end
 end
--- app/controllers/photoshoot_controller.rb
+++ app/controllers/photoshoot_controller.rb
@@ -1,48 +1,48 @@
 class PhotoshootController < ApplicationController
-  before_action :set_photoshoot, only: [:show, :edit, :update, :destroy]
+    before_action :set_photoshoot, only: [:show, :edit, :update, :destroy]
 
-  def index
-      @photoshoots = Photoshoot.all
-      render json: @photoshoots
-  end
+    def index
+        @photoshoots = Photoshoot.all
+        render json: @photoshoots
+    end
 
-  def show
-      render json: @photoshoot
-  end
+    def show
+        render json: @photoshoot
+    end
 
-  def new
-  end
+    def new
+    end
 
-  def create
-      @photoshoot = Photoshoot.new(photoshoot_params)
+    def create
+        @photoshoot = Photoshoot.new(photoshoot_params)
 
-      if @photoshoot.save
-      render json: @photoshoot, status: :created, location: @photoshoot
-      else
-      render json: @photoshoot.errors, status: :unprocessable_entity
-      end
-  end
+        if @photoshoot.save
+        render json: @photoshoot, status: :created, location: @photoshoot
+        else
+        render json: @photoshoot.errors, status: :unprocessable_entity
+        end
+    end
 
-  def edit
-  end
+    def edit
+    end
 
-  def update
-      @photoshoot.update(photoshoot_params)
-      render json: @photoshoot
-  end
+    def update
+        @photoshoot.update(photoshoot_params)
+        render json: @photoshoot
+    end
 
-  def destroy
-      @photoshoot = photoshoot.destroy
-      render json: @photoshoot
-  end
+    def destroy
+        @photoshoot = photoshoot.destroy
+        render json: @photoshoot
+    end
 
-  private 
+    private 
 
-  def set_photoshoot
-      @photoshoot = Photoshoot.find(params[:id])
-  end
+    def set_photoshoot
+        @photoshoot = Photoshoot.find(params[:id])
+    end
 
-  def photoshoot_params
-      params.require(:photoshoot).permit(:name, :portfolio_id)
-  end
+    def photoshoot_params
+        params.require(:photoshoot).permit(:name, :portfolio_id)
+    end
 end
