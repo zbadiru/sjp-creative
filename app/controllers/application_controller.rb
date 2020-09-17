c93aea9a59feaa2985c553345cbb09bb5c1e0231
zbadiru <zbadiru@live.co.uk>
2020-09-17 13:35:35 +0100

    application controller updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    def generate_token(data)
+        JWT.encode(data, ENV["secret"] )
+    end
+
+    def decode_token
+        token = request.headers["Authorization"]
+        JWT.decode(token, ENV["secret"]).first["id"]
+    end
 end
