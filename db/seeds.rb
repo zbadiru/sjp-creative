0b728176bfe6738b296402f933bf88672a38d7c7
zbadiru <zbadiru@live.co.uk>
2020-09-27 13:20:25 +0100

    top level of seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Portfolio.destroy_all
+Photoshoot.destroy_all
+Image.destroy_all
+Merchandise.destroy_all
